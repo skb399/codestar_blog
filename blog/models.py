3b4370d8e82fe87ba3e41bf5895192dba8d9f864
skb399 <stefanboakye@gmail.com>
2026-06-17 19:54:20 +0100

    change debug to false for heroku deply

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,9 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
+    
 )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.Texttfield(blank=True)
