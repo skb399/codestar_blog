ba92a738e59ea545b35e7ed0ab54ab46cd1314f5
skb399 <stefanboakye@gmail.com>
2026-06-25 15:21:12 +0100

    added post_detail view

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,16 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
+     
+    #this code is used to display the home page of the blog, which is a list of posts. 
+    # The PostList view is a class-based view that inherits from Django's generic ListView. 
+    # It retrieves all posts from the database and passes them to the index.html template for rendering.
     path('', views.PostList.as_view(), name='home'),
+    
+    # This code binds the URL pattern for the post detail page to the post_detail view function. 
+    # The post_detail view function retrieves a single post based on the slug provided in the URL
+    # and passes it to the post_detail.html template for rendering.
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    
+    
 ]
