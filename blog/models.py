29adebf82ad27d4db6b0c2ff3e66d0ba99601503
skb399 <stefanboakye@gmail.com>
2026-06-26 21:42:00 +0100

    add post comments view

--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,9 @@ class Post(models.Model):
 )
     class Meta:
         ordering = ["created_on", "author"]
+    
+    class Meta:
+        ordering = ["-created_on"]
            
     def __str__(self):
         return f"The title of this post is {self.title} written by {self.author}."      
